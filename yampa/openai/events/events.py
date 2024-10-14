af2169ece063a3106a94123f0f033ab5b3514f95
Damien Bihel <biheldamien@gmail.com>
2024-10-14 18:54:58 +0200

    fix loop

--- yampa/openai/events/events.py
+++ yampa/openai/events/events.py
@@ -39,25 +39,28 @@ def session_created_handler(event: str) -> SessionCreated:
 
 
 def make_session_update_event(tools=list[Callable]) -> SessionUpdate:
-    description = tools[0].__doc__
-    name = tools[0].__name__
-    properties = {}
-    required = []
-    for p in inspect.signature(tools[0]).parameters.values():
-        if p.annotation is str:
-            property = {"type": "string"}
-        elif get_origin(p.annotation) is Literal:
-            property = {"type": "string", "enum": get_args(p.annotation)}
-        else:
-            raise NotImplementedError(f"annotation: {p.annotation}")
-        properties[p.name] = property
-        #TODO: Support optional args
-        required.append(p.name)
+    session_tools = []
+    for tool in tools:
+        description = tool.__doc__
+        name = tool.__name__
+        properties = {}
+        required = []
+        for p in inspect.signature(tool).parameters.values():
+            if p.annotation is str:
+                property = {"type": "string"}
+            elif get_origin(p.annotation) is Literal:
+                property = {"type": "string", "enum": get_args(p.annotation)}
+            else:
+                raise NotImplementedError(f"annotation: {p.annotation}")
+            properties[p.name] = property
+            # TODO: Support optional args
+            required.append(p.name)
 
-    parameters = {"type": "object", "properties": properties, "required": required}
+        parameters = {"type": "object", "properties": properties, "required": required}
+        session_tools.append(
+            Tools(name=name, description=description, parameters=parameters)
+        )
     return SessionUpdate(
         type="session.update",
-        session=Session(
-            tools=[Tools(name=name, description=description, parameters=parameters)]
-        ),
+        session=Session(tools=session_tools),
     )
